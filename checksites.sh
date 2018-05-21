51910d9e82a688fa07820a0bcce828344a894e5d
raubin <robert.c.aubin@gmail.com>
2018-05-21 10:57:47 -0500

    Ensure site list file can be found by script.

--- checksites.sh
+++ checksites.sh
@@ -3,7 +3,7 @@
 # This script will check the response code from a list of sites to ensure
 # that they are protected. Expected response code is a 401, access denied.
 
-SITE_LIST="site_list.txt"
+SITE_LIST="${BASH_SOURCE%/*}/site_list.txt"
 
 if [[ ! -f $SITE_LIST ]];then
   echo "No site_list.txt file available! Please create one first."
