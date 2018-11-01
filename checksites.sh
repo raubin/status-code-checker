9a5f08b4abd0afbd5f76d263575aee4cd72e9039
raubin <robert.c.aubin@gmail.com>
2018-11-01 13:53:01 -0400

    Reduce output by only showing failures.

--- checksites.sh
+++ checksites.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-#
+# set -eo pipefail
+
 # This script will check the response code from a list of sites to ensure
 # that they are protected. Expected response code is a 401, access denied.
 
@@ -10,23 +11,34 @@ if [[ ! -f $SITE_LIST ]];then
   exit 1
 fi
 
+CHECKED_COUNT=0
+PASS_COUNT=0
 FAIL_COUNT=0
 
 while read url; do
+
   RESPONSE=`curl -L -s -m 5 -o /dev/null -w "%{http_code}" ${url}`
+  ((CHECKED_COUNT++))
+
   if [[ $RESPONSE == 401 ]] || [[ $RESPONSE == 403 ]]; then
-    echo "${url}: $RESPONSE OK"
+    ((PASS_COUNT++))
+    # echo "${url}: $RESPONSE OK"
   elif [[ $RESPONSE == 000 ]]; then
-    echo "${url} timed out..."
+    ((FAIL_COUNT++))
+    echo "${FAIL_COUNT}. ${url} timed out..."
   else
   	((FAIL_COUNT++))
-    echo "${url} is NOT protected: ${RESPONSE}"
+    echo "${FAIL_COUNT}. ${url} is NOT protected: ${RESPONSE}"
   fi
+
 done <$SITE_LIST
 
 echo
 echo "---------------------"
+echo "Checked: ${CHECKED_COUNT}"
+echo "Passed: ${PASS_COUNT}"
 echo "Failed: ${FAIL_COUNT}"
+echo ""
 echo
 
 exit 0
